d99e35fd9bf87821c44c6d684d8cc102eb8b0a5e
kaklakariada <kaklakariada@users.noreply.github.com>
2018-04-06 19:28:03 +0200

    Refactoring, fix warnings

--- src/download.ts
+++ src/download.ts
@@ -1,18 +1,18 @@
 import {get} from "request-promise-native";
 import {Observable} from "rxjs";
 import {config} from "../config";
-import {IBroadcastDay} from "./lib";
+import {IBroadcastDay, IDownloadTask} from "./lib";
 
 Observable.fromPromise(get(config.broadcastBaseUrl))
-  .map((res: any) => JSON.parse(res))
+  .map((res: string) => JSON.parse(res))
   .flatMap((days: IBroadcastDay[]) => Observable.from(days))
   .flatMap((day) => Observable.from(day.broadcasts))
   .map((b) => {
     const sub = config.subscriptions.find((s) => s.matches(b));
-    if(sub === undefined) {
+    if (sub === undefined) {
       return undefined;
     }
     return {subscription: sub, broadcast: b};
   })
-  .filter((task) => task !== undefined)
-  .forEach((res) => console.log(res));
+  .filter((task: IDownloadTask | undefined) => task !== undefined)
+  .forEach((task: IDownloadTask | undefined) => console.log(task));
