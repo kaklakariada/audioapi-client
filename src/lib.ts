f792fc9ab277e202d280aa5fa1e1f267a821d4ca
kaklakariada <kaklakariada@users.noreply.github.com>
2018-04-06 18:30:36 +0200

    Initial implementation

---
+++ src/lib.ts
@@ -0,0 +1,33 @@
+
+export interface IBroadcast {
+    href: string;
+    station: string;
+    id: number;
+    broadcastDay: number;
+    programKey: string;
+    title: string;
+}
+
+export interface IBroadcastDay {
+    day: number;
+    broadcasts: IBroadcast[];
+}
+
+export class BroadcastSubscription {
+    constructor(
+        public readonly title: string,
+        readonly targetFolder: string) { }
+    public matches(broadcast: IBroadcast): boolean {
+        return this.title === broadcast.title;
+    }
+}
+
+export interface IDownloadConfig {
+    broadcastBaseUrl: string;
+    subscriptions: BroadcastSubscription[];
+}
+
+export interface IDownloadTask {
+    readonly subscription: BroadcastSubscription;
+    readonly broadcast: IBroadcast;
+}
