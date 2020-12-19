ad03dae3f12e03d53986b5ce576eb46ca23ca3e6
kaklakariada <kaklakariada@users.noreply.github.com>
2020-12-19 16:50:36 +0100

    Refactoring

--- src/download.ts
+++ src/download.ts
@@ -2,14 +2,9 @@
 import * as rm from "typed-rest-client/RestClient";
 import { config } from "../config";
 import { BroadcastDownloadTask } from "./DownloadTask";
-import { IBroadcastDay, IBroadcastDetail } from "./lib";
-const restClient: rm.RestClient = new rm.RestClient("audioapi-client", config.baseUrl);
+import { IBroadcast, IBroadcastDay, IBroadcastDetail } from "./lib";
 
-function process(task: BroadcastDownloadTask): void {
-  task.getStreams().forEach((streamDownload) => {
-    console.log(streamDownload.getWgetCommand());
-  });
-}
+const restClient: rm.RestClient = new rm.RestClient("audioapi-client", config.baseUrl);
 
 async function run() {
   const data = await restClient.get<IBroadcastDay[]>("broadcasts");
@@ -17,17 +12,28 @@ async function run() {
     throw new Error("Got empty result");
   }
   data.result.forEach((day) => {
-    day.broadcasts.forEach(async (broadcast) => {
-      const subscription = config.subscriptions.find((s) => s.title === broadcast.title);
-      if (subscription) {
-        const detail = await restClient.get<IBroadcastDetail>(`broadcast/${broadcast.programKey}/${broadcast.broadcastDay}`);
-        if (detail.result === null) {
-          throw new Error("Got empty result");
-        }
-        process(new BroadcastDownloadTask(config, subscription, broadcast, detail.result.streams));
+    day.broadcasts.forEach(processBroadcast());
+  });
+}
+
+function processBroadcast(): (value: IBroadcast, index: number, array: IBroadcast[]) => void {
+  return async (broadcast) => {
+    const subscription = config.subscriptions.find((s) => s.title === broadcast.title);
+    if (subscription) {
+      const detail = await restClient.get<IBroadcastDetail>(`broadcast/${broadcast.programKey}/${broadcast.broadcastDay}`);
+      if (detail.result === null) {
+        throw new Error("Got empty result");
       }
-    });
+      process(new BroadcastDownloadTask(config, subscription, broadcast, detail.result.streams));
+    }
+  };
+}
+
+function process(task: BroadcastDownloadTask): void {
+  task.getStreams().forEach((streamDownload) => {
+    console.log(streamDownload.getWgetCommand());
   });
 }
 
 run();
+
