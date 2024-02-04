356b5c099b6150518b948e26eed16e273084ebff
kaklakariada <kaklakariada@users.noreply.github.com>
2024-02-04 10:17:12 +0100

    Add dry run option

--- python/audioapi_client/main.py
+++ python/audioapi_client/main.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 from pathlib import Path
 
@@ -21,13 +22,27 @@ def already_exists(task: DownloadTask) -> bool:
         return True
     return False
 
+def init_argparse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s [OPTION]...",
+        description="Download subscriptions using the Audio API"
+    )
+    parser.add_argument(
+        "-d", "--dry-run", action='store_true', help="Dry run: only check if new files are available but does not download them."
+    )
+    return parser
 
 def main() -> None:
+    parser = init_argparse()
+    args = parser.parse_args()
     tasks = get_download_tasks(base_url, stream_base_url, subscriptions)
     tasks = [t for t in tasks if not already_exists(t)]
-    print(f"Starting {len(tasks)} downloads...")
-    download(tasks)
-    print("Done")
+    if args.dry_run:
+        print(f"Dry run: skip download of {len(tasks)} files.")
+    else:
+        print(f"Starting {len(tasks)} downloads...")
+        download(tasks)
+        print("Done")
 
 
 if __name__ == "__main__":
